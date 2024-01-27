55e515de55ff13c26f1e45a67ce39cdbf8255b19
QQBoxy <gs1458@gmail.com>
2024-01-27 12:46:36 +0800

    Modify Line Notify

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 import axios from "axios";
 const checkHinet = () => {
     axios({
-        method: 'get',
+        method: 'post',
         url: 'https://notify-api.line.me/api/notify',
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded',
             'authorization': process.env.LINE_NOTIFY_TOKEN,
         },
-        data: {
-            message: "Github action test"
-        }
+        data: "message=Github Action Test"
     }).catch((error) => {
         console.log(error);
     });
