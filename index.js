06d93f8e5479900d859ebd8c4fd4d86ba8e03ba6
QQBoxy <gs1458@gmail.com>
2024-01-27 12:37:47 +0800

    Test Line Notify

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Hello CronBoxy", process.env.TEST_TOKEN);
+import axios from "axios";
+const checkHinet = () => {
+    axios({
+        method: 'get',
+        url: 'https://notify-api.line.me/api/notify',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'authorization': process.env.LINE_NOTIFY_TOKEN,
+        },
+        data: {
+            message: "Github action test"
+        }
+    }).catch((error) => {
+        console.log(error);
+    });
+};
+checkHinet();
